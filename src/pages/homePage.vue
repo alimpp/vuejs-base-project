c657e5a42016f8f44f77a1d0be9a62dfe4c9b86a
alimpp <hmdmpp@gmail.com>
2023-03-06 04:53:04 -0500

    Make new change

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -2,8 +2,8 @@
   <div
     class="home-page d-flex flex-column justify-content-center align-items-center pt-5 px-5 application_animation"
   >
-    <h3 class="primary_color">{{ $t("message") }}</h3>
-    <p>{{ $t("lorem") }}</p>
+    <h3 class="primary_color size_v_me">{{ $t("message") }}</h3>
+    <p class="size_v_s">{{ $t("lorem") }}</p>
   </div>
 </template>
 
